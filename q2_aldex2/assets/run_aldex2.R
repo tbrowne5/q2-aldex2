e3fd1b31e2b9e705abb50185b69695f7486f5131
tbrowne5 <44354282+tbrowne5@users.noreply.github.com>
2020-08-06 12:20:17 -0400

    Update run_aldex2.R
    Local version of ALDEx_bioc used successfully.

--- q2_aldex2/assets/run_aldex2.R
+++ q2_aldex2/assets/run_aldex2.R
@@ -17,32 +17,46 @@ map <- read.delim(inp.metadata.path, check.names=FALSE, row.names=1) # condition
 otu <- read.delim(inp.abundances.path, check.names=FALSE, row.names=1) # reads
 
 # load libraries ----------------------------------------------------------
-suppressWarnings(library(ALDEx2))
 
-# glm analysis ------------------------------------------------------------
-if (test == 'glm'){
-# conditions (ie: selex has 7 NS and 7 S)
-condsTotal <- ncol(t(otu))
-condsNumSplit <- length(unique(map[,condition]))
-condsSplitSize <- condsTotal/condsNumSplit
-# glm function (covariates, model matrix, clr function, and glm function)
-covariates <- data.frame("A" = sample(0:1, condsTotal, replace = TRUE),"B" = c(rep(0, condsSplitSize), rep(1, condsSplitSize)))
-mm <- model.matrix(~ A + B, covariates)
-x <- aldex.clr(t(otu), mm, mc.samples=1, denom="all")
-x.tt <- aldex.glm(x)
-# glm effect function (covariates, model matrix, clr function, and glm effect function)
-covariates_effect <- data.frame("A" = sample(0:1, condsTotal, replace = TRUE),"B" = c(rep(0, condsSplitSize), rep(1, condsSplitSize)),"Z" = sample(c(1,2,3), condsTotal, replace=TRUE))
-mm_effect <- model.matrix(~ A + Z + B, covariates_effect)
-x_effect <- aldex.clr(t(otu), mm_effect, mc.samples=8, denom="all")
-x.effect <- aldex.glm.effect(x_effect)
-# merging the glm effect and glm function data
-fit <- data.frame(x.effect, x.tt, check.names=F)
-}
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/AllClasses.R")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/AllGenerics.R")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/aldex.R")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/clr_corr.R")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/clr_effect.r")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/clr_function.r")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/clr_glm-tpq.R")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/clr_glm.r")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/clr_ttest.r")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/expectedDistance.aldex.r")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/glm_effect.r")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/iqlr_features.r")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/plot.aldex.r")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/plotFeature.aldex.r")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/progress.R")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/rdirichlet.r")
+source("/Users/Tyler/Desktop/ALDEx_bioc/R/stats.fast.R")
 
-# t-test analysis ---------------------------------------------------------
-else{
-fit <- aldex(t(otu), as.character(map[[condition]]),
-	     denom=denom, test=test, mc.samples=mc.samples)
+#suppressWarnings(library(ALDEx2)) # <- NEED TO TEST THIS (Rather than use local directory sources above) *********************
+
+# glm (if) or t-test (else) analysis --------------------------------------
+if(test == 'glm'){
+    #new glm addition (testing variations of conds)
+    condsTotal <- ncol(t(otu))
+    condsNumSplit <- length(unique(map[,condition]))
+    condsSplitSize <- condsTotal/condsNumSplit
+    covariates <- data.frame("A" = sample(0:1, condsTotal, replace = TRUE), "B" = c(rep(0, condsSplitSize), rep(1, condsSplitSize)))
+
+    mm <- model.matrix(~ A + B, covariates)
+    x <- aldex.clr(t(otu), mm, mc.samples=1, denom=denom)
+    xglm <- aldex.glm(x)
+    covariates_effect <- data.frame("A" = sample(0:1, condsTotal, replace = TRUE),"B" = c(rep(0, condsSplitSize), rep(1, condsSplitSize))) #,"Z" = sample(c(1,2,3), 8, replace=TRUE))
+    mm_effect <- model.matrix(~ A + B, covariates_effect) # (~ A + Z + B, covariates_effect)                                               # DO I NEED THE Z FOR THE TEST????
+    x_effect <- aldex.clr(t(otu), mm_effect, mc.samples=8, denom=denom)
+    #x_effect <- aldex.clr(t(otu), mm, mc.samples=8, denom=denom)
+    xeffect <- aldex.glm.effect(x_effect)
+    fit <- data.frame(xeffect, xglm, check.names=F)
+} else {
+    fit <- aldex(t(otu), as.character(map[[condition]]), denom=denom, test=test, mc.samples=mc.samples)
 }
 
 # final processing --------------------------------------------------------
